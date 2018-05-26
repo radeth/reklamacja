fc1a4340405ea97f9ed1416d5800dc5d8ceec147
radeth <123radeth@gmail.com>
2018-05-26 10:31:27 +0200

    jd2

--- js.js
+++ js.js
@@ -1,23 +1,60 @@
 'use strict';
-$(document).ready(function(){
-    
-    console.log('test');
-    const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    removeValue();
-    
+$(document).ready(function() {
+
+  console.log('test');
+  removeValue();
+  mailValidation();
+  phoneValidation();
+  cardValidation();	
+
+  
+
 })
 
-function mailValidation(){
-    $('input').on('click',function(){
-        console.log('test');
-        $(this).removeAttr('value');
-    })
-       
+function removeValue() {
+  $('input').on('click', function() {
+    $(this).removeAttr('value');
+  })
+
+}
+
+function mailValidation() {
+  let regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  $('input:nth-of-type(1)').on('keyup', function() {
+    console.log(regExp.test($('input:nth-of-type(1)').val()));
+    if (regExp.test($('input:nth-of-type(1)').val()) == false) {
+      $(this).css('border-color', 'red');
+    }
+    if (regExp.test($('input:nth-of-type(1)').val()) == true) {
+      $(this).css('border-color', '#aaa');
+    }
+  })
 }
 function phoneValidation(){
-    
+    let regExp =/\d/;
+	$('input:nth-of-type(2)').on('keyup', function() {
+    console.log(regExp.test($('input:nth-of-type(2)').val()));
+    if (regExp.test($('input:nth-of-type(2)').val()) == false) {
+      $(this).css('border-color', 'red');
+    }
+    if (regExp.test($('input:nth-of-type(2)').val()) == true) {
+      $(this).css('border-color', '#aaa');
+    }
+  })
 }
 function cardValidation(){
-    
+    let regExp =/\d/;
+	$('input:nth-of-type(3)').on('keyup', function() {
+    console.log(regExp.test($('input:nth-of-type(3)').val()));
+    if (regExp.test($('input:nth-of-type(3)').val()) == false) {
+      $(this).css('border-color', 'red');
+    }
+    if (regExp.test($('input:nth-of-type(3)').val()) == true) {
+      $(this).css('border-color', '#aaa');
+    }
+  })
 }
-                  
+
+
+
+
